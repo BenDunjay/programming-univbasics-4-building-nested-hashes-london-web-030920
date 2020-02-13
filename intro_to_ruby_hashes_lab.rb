1a84f823077418a3d19ca55f605836de3521d98e
Ben Dunjay <bendunjay@outlook.com>
2020-02-13 09:32:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,10 +30,10 @@ def monopoly_with_third_tier
          :three_pieces_owned => 100,
          :four_pieces_owned => 200 }
         :names => {
-          :reading => {}
-          :pennsylvania => {}
-          :b_and_o => {}
-          :shortline => {}
+          :reading_railroad => {}
+          :pennsylvania_railroad => {}
+          :b_and_o_railroad => {}
+          :shortline_railroad => {}
           
         } 
          
